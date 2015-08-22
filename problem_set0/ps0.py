c1ba3a3018f1a40510e049719177e0541cf2c92e
rnparks <rnparks@gmail.com>
2015-08-22 08:33:10 -0400

    problem set 0 complete

---
+++ problem_set0/ps0.py
@@ -0,0 +1,7 @@
+#MIT 6.00SC
+#problem set 0
+
+dob = raw_input("Please enter your date of birth (mm/dd/yy): ")
+last_name = raw_input("Please enter your late name: ")
+print("%s %s" % (last_name, dob))  # could have written print last_name + " " + dob
+
